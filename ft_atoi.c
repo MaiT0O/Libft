72e1c49b2cc39042cb31328d89525ce84fe3d708
Erwann Bansse <ebansse@c2r6p6.42perpignan.fr>
2025-03-10 14:36:02 +0100

    ft_atoi optimised

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ebansse <ebansse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:39:19 by ebansse           #+#    #+#             */
-/*   Updated: 2024/11/08 14:40:56 by ebansse          ###   ########.fr       */
+/*   Updated: 2025/03/10 14:35:35 by ebansse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,25 @@
 
 int	ft_atoi(const char *str)
 {
+	int	neg;
 	int	i;
-	int	s;
-	int	res;
+	int	num;
 
 	i = 0;
-	s = 1;
-	res = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
-		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+	neg = 1;
+	num = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
-			s = -1;
+			neg *= -1;
 		i++;
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (str[i] >= 48 && str[i] <= 57)
 	{
-		res = (res * 10) + (str[i] - '0');
+		num = num * 10 + (str[i] - 48);
 		i++;
 	}
-	return (res * s);
+	return (num * neg);
 }
